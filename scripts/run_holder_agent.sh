656b94c41a546b1eb43b6682a002fb5c0bf1c7d7
yunxi.zhang <yunxi.zhang@accenture.com>
2021-02-24 14:38:23 +0000

    add run holder agent file

---
+++ scripts/run_holder_agent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PORTS="8000:8000 8001:8001" \
+./run_docker start \
+-l Holder \
+-it http 0.0.0.0 8000 \
+-ot http \
+--admin 0.0.0.0 8001 \
+--admin-insecure-mode \
+-e http://$1:8000/ \
+--genesis-url http://$2:9000/genesis \
+--wallet-type indy \
+--wallet-name holderwallet \
+--wallet-key holderkey \
+--seed holder00000000000000000000000000  \
+--log-level 'info'  \
+--auto-provision \
+--auto-accept-invites \
+--auto-accept-requests \
+--auto-respond-messages \
+--auto-ping-connection
